5f2ffed3d61f62a762d630bb0c698e78ee96d2be
johnfox17 <johnfox17@gmail.com>
2024-05-29 06:37:36 -0700

    Initialized repo

---
+++ src/main.py
@@ -0,0 +1,51 @@
+import sys
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import fuzzyDiffusionFilterPDDO
+import constants
+
+def main():
+    
+    for iThreshold in constants.THRESHOLDS:
+        if sys.platform.startswith('linux'):
+            pathToLena = \
+                '../data/simData/noisyCircle'
+                #'../data/simData/noisyLena.png'
+                # '../data/simData/Lena.png'
+            pathToMembershipFunction = '../data/simData/triangularMembershipFunction.csv'
+
+        else:
+            pathToLena = \
+                '..\\data\\simData\\noisyCircle.png'
+                #'..\\data\\simData\\noisyLena.png'
+                #'..\\data\\simData\\Lena.png'
+            pathToMembershipFunction = '..\\data\\simData\\triangularMembershipFunction.csv'
+
+        image = cv2.imread(pathToLena)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        fuzzyFilter = fuzzyDiffusionFilterPDDO.fuzzyDiffusionFilterPDDO(image,pathToMembershipFunction,iThreshold)
+        fuzzyFilter.solve()
+
+
+
+
+
+    imagePlot = plt.imshow(image, cmap='gray', vmin=0, vmax=255)
+    plt.show()
+    #signals = createSignals.createSignals()
+    #signals.solve()
+    
+
+    #np.savetxt('..\\data\\localSmoothness.csv',  fuzzyFilter.localSmoothness, delimiter=",")
+    #np.savetxt('..\\data\\generalAverage.csv',  fuzzyFilter.generalAverage, delimiter=",")
+    #np.savetxt('..\\data\\denoisedImages.csv',  fuzzyFilter.denoisedImages, delimiter=",")
+    #np.savetxt('..\\data\\neighboringPixels.csv',  fuzzyFilter.neighboringPixels, delimiter=",")
+    print(fuzzyFilter.neighboringPixels)
+    #np.savetxt('/home/doctajfox/Documents/Thesis/1DPDDOKernels/data/xi2_2.csv', PDDOKernel2_2.xis, delimiter=",")
+
+
+if __name__ == "__main__":
+    main()
+
